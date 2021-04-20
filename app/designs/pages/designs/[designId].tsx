04e9a3ae3d19861d09b3fdf43d8b8abdb6e35766
Max Isom <hi@maxisom.me>
2021-04-20 19:22:00 -0400

    Add images to design page

--- app/designs/pages/designs/[designId].tsx
+++ app/designs/pages/designs/[designId].tsx
@@ -1,20 +1,22 @@
-import {Suspense} from 'react';
+import {Suspense, useState} from 'react';
 import Layout from 'app/layouts/Layout';
 import {
 	Link,
 	useQuery,
 	useParam,
 	BlitzPage,
-	useSession
+	useSession,
+	Image
 } from 'blitz';
 import {Heading, Text, Grid, Container, Box, Spacer, Button, VStack, Wrap, WrapItem, HStack, IconButton} from '@chakra-ui/react';
 import getDesign from 'app/designs/queries/getDesign';
-import {EditIcon} from '@chakra-ui/icons';
+import {ChevronLeftIcon, ChevronRightIcon, EditIcon} from '@chakra-ui/icons';
 import getUploadPreviewUrl from 'utils/get-upload-preview-url';
 
 const currencyFormat = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'});
 
 export const Design = () => {
+	const [currentPictureIndex, setCurrentPictureIndex] = useState(0);
 	const designId = useParam('designId', 'number');
 	const [design] = useQuery(getDesign, {where: {id: designId}});
 
@@ -22,8 +24,48 @@ export const Design = () => {
 	const canEditDesign = session.roles?.includes('ADMIN') === true || design.userId === session.userId;
 
 	return (
-		<Grid templateColumns="40% 1fr" gap={24}>
-			<img src={getUploadPreviewUrl(design.stitchFileId, 'svg')} width="100%"/>
+		<Grid templateColumns={{sm: '1fr', md: '40% 1fr'}} templateRows={{sm: '1fr 1fr', md: '1fr'}} gap={24}>
+			<VStack>
+				<Box h="30vh" d="flex" position="relative" w="full">
+					<Image src={design.pictures.length > 0 ? getUploadPreviewUrl(design.pictures[currentPictureIndex].id) : getUploadPreviewUrl(design.stitchFileId, 'svg')} objectFit="contain" layout="fill"/>
+				</Box>
+
+				{
+					design.pictures.length > 0 && (
+						<HStack w="full">
+							<IconButton
+								onClick={() => {
+									setCurrentPictureIndex(i => i - 1);
+								}}
+								disabled={currentPictureIndex === 0}
+								icon={<ChevronLeftIcon/>}
+								aria-label="Previous image"
+								variant="outline"/>
+
+							{Array.from(Array.from({length: 3}).keys()).map(i =>
+								design.pictures.length > (i + currentPictureIndex) ? (
+									<Box flexBasis={0} flexGrow={1} d="flex" position="relative" h="20vh" key={i}>
+										<Image src={getUploadPreviewUrl(design.pictures[i + currentPictureIndex].id)} layout="fill" objectFit="contain"/>
+									</Box>
+								) : (
+									<Box flexBasis={0} flexGrow={1} d="flex" position="relative" h="20vh" key={i}/>
+								)
+							)}
+
+							<IconButton
+								icon={<ChevronRightIcon/>}
+								aria-label="Next image"
+								variant="outline"
+								disabled={currentPictureIndex === design.pictures.length - 1}
+								onClick={() => {
+									setCurrentPictureIndex(i => i + 1);
+								}}
+							/>
+						</HStack>
+					)
+				}
+
+			</VStack>
 
 			<VStack align="flex-start">
 				<HStack>
@@ -41,15 +83,11 @@ export const Design = () => {
 
 				</HStack>
 
-				<Heading size="md">{currencyFormat.format(design.price)}</Heading>
+				<Heading size="md" mb={2}>{currencyFormat.format(design.price)}</Heading>
 
-				<Spacer height={6} flexGrow={0}/>
-
-				<Text>
+				<Text mb={6}>
 					{design.description}
 				</Text>
-
-				<Spacer flexGrow={1}/>
 
 				<Wrap spacing={4}>
 					<WrapItem>
